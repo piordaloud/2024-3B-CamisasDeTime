193b4589b1c9d8820a077fd5152e3ab107c3cc15
gian SaydeN <106679444+piordaloud@users.noreply.github.com>
2024-09-12 10:31:15 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
-print("""
-      
-Camisa de time tailandesa
-      
-""")
+import os
 
-print('1. Cadastro de camisas')
-print('2. Listar camisas')
-print('3. Ativar camisas')
-print('4. Sair da aplicação')
+def mostra_título():
+    print("""
+        
+    Camisa de time tailandesa
+        
+    """)
 
-opcao_escolhida = int(input('Escolha uma opcao: '))
-print('Você escolheu a opção: ', opcao_escolhida)
+def mostra_escolhas():
+    print('1. Cadastro de camisas')
+    print('2. Listar camisas')
+    print('3. Ativar camisas')
+    print('4. Sair da aplicação')
 
-if opcao_escolhida == 1:
-    print('Cadastrar camisa')
-elif opcao_escolhida == 2:
-    print('Listar camisas')
-elif opcao_escolhida == 3:
-    print('Ativar/desativar camisa')
-else:
-    print('Saindo da aplicação')
+def escolhe_opcao():
+    opcao_escolhida = int(input('Escolha uma opcao: '))
+    print('Você escolheu a opção: ', opcao_escolhida)
+
+    def finalizar_programa():
+        os.system('cls')
+        print('finalizando programa')
+
+
+    if opcao_escolhida == 1:
+        print('Cadastrar camisa')
+    elif opcao_escolhida == 2:
+        print('Listar camisas')
+    elif opcao_escolhida == 3:
+        print('Ativar/desativar camisa')
+    else:
+        finalizar_programa()
+
+def main():
+    mostra_título()
+    mostra_escolhas()
+    escolhe_opcao()
+
+if __name__ == '__main__':
+    main()
