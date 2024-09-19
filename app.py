ff69c335b4d991897646dc858b7274373c3f22c8
gian SaydeN <106679444+piordaloud@users.noreply.github.com>
2024-09-19 10:29:16 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -14,22 +14,47 @@ def mostra_escolhas():
     print('4. Sair da aplicação')
 
 def escolhe_opcao():
-    opcao_escolhida = int(input('Escolha uma opcao: '))
-    print('Você escolheu a opção: ', opcao_escolhida)
+    try:
+        opcao_escolhida = int(input('Escolha uma opcao: '))
+        print('Você escolheu a opção: ', opcao_escolhida)
 
-    def finalizar_programa():
-        os.system('cls')
-        print('finalizando programa')
+        if opcao_escolhida == 1:
+            print('Cadastrar camisa')
+        elif opcao_escolhida == 2:
+            print('Listar camisas')
+        elif opcao_escolhida == 3:
+            print('Ativar/desativar camisa')
+        elif opcao_escolhida == 4:
+            finalizar_programa
+        else:
+           opcao_invalida()
+    except:
+        opcao_invalida()
 
+def cadastrar_camisas():
+    os.system('cls')
+    print(' Cadastrando Camisa... ')
+    nome_camisa = input('Digite o nome da camisa')
+    camisas.append(nome_camisa)
+    print(f'O {nome_camisa} foi adicionado (a) as camisas de time')
+    input('Digite qualquer tecla para voltar')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar camisa')
-    elif opcao_escolhida == 2:
-        print('Listar camisas')
-    elif opcao_escolhida == 3:
-        print('Ativar/desativar camisa')
-    else:
-        finalizar_programa()
+def mostrar_camisas():
+    os.system('cls')
+    print('Lista de camisas')
+
+    for camisa
+
+def finalizar_programa():
+    os.system('cls')
+    print('finalizando programa')
+
+def opcao_invalida():
+    print('Esse caracter não é permitido')
+    input('digite qualquer tecla')
+    main()
+
 
 def main():
     mostra_título()
